c661525ee54c831ae1e9fe98739909da61c73dfd
AditInx <adit.s@inhertix.com>
2025-03-19 18:18:03 +0530

    emitting events

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 import express from 'express';
-import {createServer} from 'node:http';
+import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
-import { dirname,join } from 'node:path';
+import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
-import http from "http"
-http.createServer()
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/',(req,res)=>{
+// Serve index.html
+app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection',(socket)=>{
-    console.log('a user connected');
-    socket.on('disconnect',()=>{
-        console.log('user disconnected');
-    })
-})
+// Handle socket connections
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);  // Broadcast the message to all clients
+    });
 
-server.listen(7000,()=>{
-    console.log(`server running at http://localhost:7000`);
-})
+    socket.off('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(7000, () => {
+    console.log(`Server running at http://localhost:7000`);
+});
